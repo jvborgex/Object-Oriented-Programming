fa5cc79991b9f26ac05cd5761ecd4b38225c0f88
João Victor Borges <136815016+jvborgex@users.noreply.github.com>
2023-12-12 17:26:35 -0300

    classe-cliente.py

---
+++ classe-cliente.py
@@ -0,0 +1,50 @@
+class Cliente:
+    def __init__(self, nome, email, senha, telefone, idioma):
+        self._nome = nome
+        self._email = email
+        self._senha = senha
+        self._telefone = telefone
+        self._idioma = idioma
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = email
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @senha.setter
+    def senha(self, senha):
+        self._senha = senha
+
+    @property
+    def telefone(self):
+        return self._telefone
+
+    @telefone.setter
+    def telefone(self, telefone):
+        self._telefone = telefone
+
+    @property
+    def idioma(self):
+        return self._idioma
+
+    @idioma.setter
+    def idioma(self, idioma):
+        self._idioma = idioma
+
+    def __str__(self):
+      return f'Nome: {self.nome}\nEmail: {self.email}\nSenha: {self.senha}\nTelefone: {self.telefone}\nIdioma: {self.idioma}'
