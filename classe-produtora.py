93e429030d0b18c35d00f4201a7eee7f3b0bf786
João Victor Borges <136815016+jvborgex@users.noreply.github.com>
2023-12-12 17:24:36 -0300

    classe-produtora.py

---
+++ classe-produtora.py
@@ -0,0 +1,37 @@
+class Produtora:
+    def __init__(self, nome, sede, dono, endereco):
+        self.__nome = nome
+        self.__sede = sede
+        self.__dono = dono
+        self.__endereco = endereco
+
+    @property
+    def nome(self):
+        return self.__nome
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def sede(self):
+        return self.__sede
+    @sede.setter
+    def sede(self, sede):
+        self.__sede = sede
+
+    @property
+    def dono(self):
+        return self.__dono
+    @dono.setter
+    def dono(self, dono):
+        self.__dono = dono
+
+    @property
+    def endereco(self):
+        return self.__endereco
+    @endereco.setter
+    def endereco(self, endereco):
+        self.__endereco = endereco
+
+    def __str__(self):
+        return f'Nome: {self.__nome}\nSede: {self.__sede}\nDono: {self.__dono}\nEndereço: {self.__endereco}'
