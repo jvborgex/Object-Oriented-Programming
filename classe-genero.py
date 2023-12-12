552e365a47fca28ac0dce6a3f6f9ff4c44711b76
João Victor Borges <136815016+jvborgex@users.noreply.github.com>
2023-12-12 17:25:07 -0300

    classe-genero.py

---
+++ classe-genero.py
@@ -0,0 +1,14 @@
+class Genero:
+    def __init__(self, tipo):
+        self._tipo = tipo
+
+    @property
+    def tipo(self):
+        return self._tipo
+
+    @tipo.setter
+    def tipo(self, tipo):
+        self._tipo = tipo
+
+    def __str__(self):
+        return f'Genero: {self._tipo}'
