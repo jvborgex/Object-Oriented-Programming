48174ca16f5794bcb04f6086739f4a697fc5f79d
João Victor Borges <136815016+jvborgex@users.noreply.github.com>
2023-12-12 17:29:23 -0300

    classe-avaliacao.py

---
+++ classe-avaliacao.py
@@ -0,0 +1,38 @@
+class Avaliacao:
+    def __init__(self, nota, comentario):
+        self._nota = nota
+        self._comentario = comentario
+        self._filme = None
+        self._usuarioApp = None
+
+    @property
+    def nota(self):
+        return self._nota
+
+    @nota.setter
+    def nota(self, nota):
+        self._nota = nota
+
+    @property
+    def comentario(self):
+        return self._comentario
+
+    @comentario.setter
+    def comentario(self, comentario):
+        self._comentario = comentario
+
+    @property
+    def filme(self):
+        return self._filme
+
+    @filme.setter
+    def filme(self, filme):
+        self._filme = filme
+
+    @property
+    def usuarioApp(self):
+        return self._usuarioApp
+
+    @usuarioApp.setter
+    def usuarioApp(self, usuarioApp):
+        self._usuarioApp = usuarioApp
